82b8788e3cd6dd9c227b82420887b8b1c59bda3e
AnisBenFerhat <anisbenferhat@gmail.com>
2023-12-28 12:19:45 +0100

    Feat(SuperQuality): Create SuperQuality section
    - Added SuperQuality section showcasing shoes
    - Modified the Button component, making the imageURL optional as we have
    button without icon
    
    New Files:
    - src/sections/SuperQuality.tsx

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 interface ButtonProps {
   label: string;
-  iconUrl: string;
+  iconUrl?: string;
 }
 
 const Button: React.FC<ButtonProps> = ({ label, iconUrl }) => {
   return (
     <button className='flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-x-coral-red'>
       {label}
-      <img src={iconUrl} alt='arrow right icon' className='ml-2 rounded-full w-5 h-5' />
+      {iconUrl && <img src={iconUrl} alt='arrow right icon' className='ml-2 rounded-full w-5 h-5' />}
     </button>
   );
 };

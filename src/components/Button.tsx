583c6f202a8cfad7a905f062bf12b7991d7e31b8
AnisBenFerhat <anisbenferhat@gmail.com>
2023-12-28 15:23:36 +0100

    Feat(SpecialOffers): Edit and style section
    - Updated: Button component to accept new props
    - Implemented enhanced Button component in SpecialOffers section

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,15 +1,22 @@
-import React from 'react';
-
 interface ButtonProps {
   label: string;
   iconUrl?: string;
+  backgroundColor?: string;
+  textColor?: string;
+  borderColor?: string;
+  fullWidth?: boolean;
 }
 
-const Button: React.FC<ButtonProps> = ({ label, iconUrl }) => {
+const Button: React.FC<ButtonProps> = ({ label, iconUrl, backgroundColor, textColor, borderColor, fullWidth }) => {
   return (
-    <button className='flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-x-coral-red'>
+    <button
+      className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
+      ${
+        backgroundColor ? `${backgroundColor} ${textColor} ${borderColor}` : 'bg-coral-red text-white border-coral-red'
+      } rounded-full ${fullWidth ? 'w-full' : ''}`}
+    >
       {label}
-      {iconUrl && <img src={iconUrl} alt='arrow right icon' className='ml-2 rounded-full w-5 h-5' />}
+      {iconUrl && <img src={iconUrl} alt='arrow right icon' className='ml-2 rounded-full bg-white w-5 h-5' />}
     </button>
   );
 };

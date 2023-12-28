803092d8d50657cb0e7435169bef0392a0bd731e
AnisBenFerhat <anisbenferhat@gmail.com>
2023-12-28 14:21:29 +0100

    Feat(Services): Implemented and integrated component
    - Created ServiceCard component to display service information
    - Updated Services section to dynamically render ServiceCards for
    each service item
    - Used the ServiceCard component in the Services section to display
    service details

--- src/sections/SuperQuality.tsx
+++ src/sections/SuperQuality.tsx
@@ -3,7 +3,10 @@ import Button from '../components/Button';
 
 const SuperQuality = () => {
   return (
-    <section id='about-us' className='flex justify-between items-center max-lg:flex-col gap-10 w-full max-container'>
+    <section
+      id='about-us'
+      className='padding flex justify-between items-center max-lg:flex-col gap-10 w-full max-container'
+    >
       <div className='flex flex-1 flex-col'>
         <h2 className='font-palanquin capitalize text-4xl lg:max-w-lg font-bold'>
           We Provide You
